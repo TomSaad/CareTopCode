a0f9120625d29299766294f151462afbbb807c08
TomSaad <tomsaad5739@gmail.com>
2018-04-10 00:19:19 -0400

    humidity/temp sensors worked on arduino; python/pi untested; uvb untested as of yet, needs i2c?

---
+++ sensors/arduinotest/humtemp/humtemp.ino
@@ -0,0 +1,51 @@
+#include <Adafruit_Sensor.h>
+
+#include "DHT.h"
+
+#define DHTPIN 8
+#define DHTTYPE DHT11 //have dht11 type; not 22 or 21
+//int HTout = 8; //output from humidity/temp sensor on pin 8
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 output:");
+
+  dht.begin();
+  
+  //pinMode(HTout, INPUT);
+  //hahaha im funny the output variable is really input hahahaha please laugh
+}
+
+void loop() {
+  /*
+   * int HTreading = digitalRead(HTout);
+   * Serial.println(HTreading);
+   */
+
+   float hum = dht.readHumidity();
+   float temp = dht.readTemperature();
+   float fahr = dht.readTemperature(true); //isFahrenheit = true
+
+   if (isnan(hum) || isnan(temp) || isnan(fahr)) {
+    Serial.println("Failed read.");
+    return;
+   }
+
+   float hic = dht.computeHeatIndex(temp, hum, false); //isFahrenheit = false
+   float hif = dht.computeHeatIndex(fahr, hum);
+
+   Serial.print("Humidity: ");
+   Serial.print(hum);
+   Serial.print("%\t Temperature: ");
+   Serial.print(temp);
+   Serial.print("C (");
+   Serial.print(fahr);
+   Serial.print("F) \t Heat Index: ");
+   Serial.print(hic);
+   Serial.print("*C (");
+   Serial.print(hif);
+   Serial.println("*F)");
+   delay(1000);
+}
---
+++ sensors/humtemp.py
@@ -0,0 +1,67 @@
+# deal with gpio here; need 5 sensor lines, power ground can be shared.
+
+import sys
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+
+###GPIO SETUP
+GPIO.setwarnings(False)
+
+#Use the BCM numbers not the direct pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Assigned just going down left side
+UVB_1_PIN = 4
+HUMTMP_1_PIN = 17
+HUMTMP_2_PIN = 27
+HUMTMP_3_PIN = 22
+HUMTMP_4_PIN = 5
+HUMTMP_5_PIN = 6
+
+GPIO.setup(UVB_1_PIN, GPIO.OUT)
+GPIO.setup(HUMTMP_1_PIN, GPIO.OUT)
+GPIO.setup(HUMTMP_2_PIN, GPIO.OUT)
+GPIO.setup(HUMTMP_3_PIN, GPIO.OUT)
+GPIO.setup(HUMTMP_4_PIN, GPIO.OUT)
+GPIO.setup(HUMTMP_5_PIN, GPIO.OUT)
+
+uvsensors = [UVB_1_PIN]
+uvb = [] * len(uvsensors)
+
+htsensors = [HUMTMP_1_PIN, HUMTMP_2_PIN, HUMTMP_3_PIN, HUMTMP_4_PIN, HUMTMP_5_PIN]
+humtemp = [] * len(htsensors)
+
+
+def read(pin):
+    """
+        takes in pin for sensor desired to read
+        returns tuple of (humidity, temperature), using DHT library read function
+    """
+    # read_retry from DHT library attempts to read until valid tuple of (humidity, temperature) is returned.
+    # if failed to do so in certain number of tries or time, returns (None, None).
+    # read_retry arguments:
+    # read_retry(sensor, pin, retries=15, delay_seconds=2, platform=None)
+    # we use DHT11, so sensor is always 11
+    return Adafruit_DHT.read_retry(11, pin)
+
+def readHumidity(pin):
+    """
+        takes in pin for sensor desired to read
+        returns humidity value (first of tuple (humidity, temperature))
+    """
+    return Adafruit_DHT.read_retry(11, pin)[0]
+
+def readTemperature(pin):
+    """
+        takes in pin for sensor desired to read
+        returns temperature value (second of tuple (humidity, temperature))
+    """
+    return Adafruit_DHT.read_retry(11, pin)[1]
+
+def readAll():
+    for uvb_sensor in range(1, len(uvsensors)):
+        uvb[uvb_sensor] = readUVB(uvsensors[uvb_sensor])  # pass in pin of sensor
+
+    for ht_sensor in range(1, len(htsensors)):
+        humtemp[ht_sensor] = read(htsensors[ht_sensor])  # pass in pin of sensor
+
